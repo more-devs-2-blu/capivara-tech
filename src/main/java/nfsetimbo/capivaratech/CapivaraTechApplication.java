a170a064adba6f35995853f1a6d6cd82584d649e
Dikendev <diken.dev@gmail.com>
2023-03-11 23:46:19 -0300

    initial commit

---
+++ src/main/java/nfsetimbo/capivaratech/CapivaraTechApplication.java
@@ -0,0 +1,13 @@
+package nfsetimbo.capivaratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapivaraTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapivaraTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nfsetimbo/capivaratech/CapivaraTechApplicationTests.java
@@ -0,0 +1,13 @@
+package nfsetimbo.capivaratech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapivaraTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
