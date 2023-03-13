3011f64ed9526c428b752723857fe91b7405e6ef
Dikendev <diken.dev@gmail.com>
2023-03-13 17:26:34 -0300

    fixed CpfCnpj class

--- src/main/java/nfsetimbo/capivaratech/bean/CpfCnpj.java
+++ src/main/java/nfsetimbo/capivaratech/bean/CpfCnpj.java
@@ -1,27 +1,28 @@
 package nfsetimbo.capivaratech.bean;
 
-
 import jakarta.validation.constraints.Size;
-import jakarta.xml.bind.annotation.XmlElement;
-import org.hibernate.validator.constraints.br.CNPJ;
-import org.hibernate.validator.constraints.br.CPF;
+import lombok.Getter;
+import lombok.Setter;
+import nfsetimbo.capivaratech.soap.TypeBase;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
-public class CpfCnpj {
+@Getter
+@Setter
+public class CpfCnpj extends TypeBase {
 
-    @CNPJ
-    @CPF
-    @Size(min = 11, max = 14)
-    public int cpfcnpj;
+    protected int cpfCnpj;
 
     public CpfCnpj(){
     }
 
-    @XmlElement(name = "cpfcnpj")
-    public int getCpfCnpj() {
-        return cpfcnpj;
+    public CpfCnpj(Element root) {
+        Node nodeCpfCnpj = super.extractNodeByTag(root.getElementsByTagName("CpfCnpj" ));
+        this.cpfCnpj = nodeCpfCnpj != null ? Integer.parseInt(nodeCpfCnpj.getTextContent()) : 0;
     }
-
-    public void setCpfCnpj(int cpfCnpj) {
-        this.cpfcnpj = cpfcnpj;
+    public String generateCpfcnpj() {
+        return "<cpfcnpj>"
+                    + cpfCnpj +
+                "</cpfcnpj>";
     }
 }
