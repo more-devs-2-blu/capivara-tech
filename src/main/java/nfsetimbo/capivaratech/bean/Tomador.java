f3c827d51735e6b8e631f4d64b832a5a6aabc273
Dikendev <diken.dev@gmail.com>
2023-03-13 17:29:17 -0300

    added and fixed methods

--- src/main/java/nfsetimbo/capivaratech/bean/Tomador.java
+++ src/main/java/nfsetimbo/capivaratech/bean/Tomador.java
@@ -1,9 +1,24 @@
 package nfsetimbo.capivaratech.bean;
 
-import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+import nfsetimbo.capivaratech.bean.enums.Tipo;
+import nfsetimbo.capivaratech.soap.TypeBase;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
-public class Tomador implements Serializable {
+@Getter
+@Setter
+public class Tomador extends TypeBase {
 
     private Tipo tipo;
     private CpfCnpj cpfCnpj;
+
+    public Tomador(Element root) {
+        Node nodeTipo = super.extractNodeByTag(root.getElementsByTagName("Tipo"));
+        Node nodeCpfCnpj = super.extractNodeByTag(root.getElementsByTagName("CpfCnpj"));
+
+        this.tipo = nodeTipo!=null? Tipo.valueOf(nodeTipo.getTextContent()) : null;
+        this.cpfCnpj = nodeCpfCnpj!=null ? new CpfCnpj((Element) nodeCpfCnpj) : null;
+    }
 }
