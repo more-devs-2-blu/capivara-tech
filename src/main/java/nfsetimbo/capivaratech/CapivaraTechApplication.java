7ada0ea18d35d9ce5d29caa1f6ae97a57b65aeba
Dikendev <diken.dev@gmail.com>
2023-03-14 10:46:46 -0300

    implemented main application web service

--- src/main/java/nfsetimbo/capivaratech/CapivaraTechApplication.java
+++ src/main/java/nfsetimbo/capivaratech/CapivaraTechApplication.java
@@ -1,13 +1,22 @@
 package nfsetimbo.capivaratech;
 
+import nfsetimbo.capivaratech.service.WebClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CapivaraTechApplication {
+public class CapivaraTechApplication  implements CommandLineRunner {
 
+	@Autowired
+	WebClientService webClientService;
 	public static void main(String[] args) {
 		SpringApplication.run(CapivaraTechApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		webClientService.sendXmlRequest();
+	}
 }
