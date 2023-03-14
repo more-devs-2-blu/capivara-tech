fb9c4fa86ae44fc6d3eb7cf70997a1882d9e4648
Dikendev <diken.dev@gmail.com>
2023-03-14 10:41:06 -0300

    fixed cpfcnpj

--- src/main/java/nfsetimbo/capivaratech/bean/CpfCnpj.java
+++ src/main/java/nfsetimbo/capivaratech/bean/CpfCnpj.java
@@ -1,28 +1,18 @@
 package nfsetimbo.capivaratech.bean;
 
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import nfsetimbo.capivaratech.soap.TypeBase;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
 @Getter
 @Setter
-public class CpfCnpj extends TypeBase {
+public class CpfCnpj {
 
     protected int cpfCnpj;
 
     public CpfCnpj(){
     }
 
-    public CpfCnpj(Element root) {
-        Node nodeCpfCnpj = super.extractNodeByTag(root.getElementsByTagName("CpfCnpj" ));
-        this.cpfCnpj = nodeCpfCnpj != null ? Integer.parseInt(nodeCpfCnpj.getTextContent()) : 0;
-    }
-    public String generateCpfcnpj() {
-        return "<cpfcnpj>"
-                    + cpfCnpj +
-                "</cpfcnpj>";
+    public CpfCnpj(int cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
     }
 }
