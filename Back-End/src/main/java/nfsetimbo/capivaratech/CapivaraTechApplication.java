e5240616a0b3865f26780f7d54c8199df0d6f174
Dikendev <diken.dev@gmail.com>
2023-03-14 14:58:44 -0300

    fixed main application

--- Back-End/src/main/java/nfsetimbo/capivaratech/CapivaraTechApplication.java
+++ Back-End/src/main/java/nfsetimbo/capivaratech/CapivaraTechApplication.java
@@ -1,22 +1,12 @@
 package nfsetimbo.capivaratech;
 
-import nfsetimbo.capivaratech.service.WebClientService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CapivaraTechApplication  implements CommandLineRunner {
-
-	@Autowired
-	WebClientService webClientService;
+public class CapivaraTechApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CapivaraTechApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		webClientService.sendXmlRequest();
-	}
 }
