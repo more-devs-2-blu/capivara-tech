e030f1f51dae929d0f7995183ade96d0214a5804
Dikendev <diken.dev@gmail.com>
2023-03-14 10:48:16 -0300

    added valores Nfse

---
+++ src/main/java/nfsetimbo/capivaratech/bean/ValoresNfse.java
@@ -0,0 +1,30 @@
+package nfsetimbo.capivaratech.bean;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+import java.io.Serializable;
+
+
+@XmlType(propOrder = {"valor_total"})
+public class ValoresNfse implements Serializable {
+
+    private Double valorTotal;
+
+    public ValoresNfse() {
+    }
+
+    @XmlElement(name = "valor_total")
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+
+
+
+
+}
