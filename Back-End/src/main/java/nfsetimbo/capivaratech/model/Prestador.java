99646b6c25ac9649d08e75dd7743cec67a53c309
Dikendev <diken.dev@gmail.com>
2023-03-16 10:42:37 -0300

    added Prestador class an xml attr

--- Back-End/src/main/java/nfsetimbo/capivaratech/model/Prestador.java
+++ Back-End/src/main/java/nfsetimbo/capivaratech/model/Prestador.java
@@ -1,33 +1,25 @@
 package nfsetimbo.capivaratech.model;
 
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.*;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
+@XmlType(propOrder = {"cpfcnpj", "cidade"})
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Prestador implements Serializable {
 
-    protected CpfCnpj cpfCnpj;
+    @XmlElement(name = "cpfcnpj")
+    private int cpfCnpj;
+    @XmlElement(name = "cidade")
     protected int cidade;
 
-    public Prestador(CpfCnpj cpfCnpj, int cidade) {
+    public Prestador(int cpfCnpj, int cidade) {
         this.cpfCnpj = cpfCnpj;
         this.cidade = cidade;
     }
 
-    public CpfCnpj getCpfCnpj() {
-        return cpfCnpj;
-    }
 
-    public void setCpfCnpj(CpfCnpj cpfCnpj) {
-        this.cpfCnpj = cpfCnpj;
-    }
-
-    public int getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(int cidade) {
-        this.cidade = cidade;
-    }
 }
