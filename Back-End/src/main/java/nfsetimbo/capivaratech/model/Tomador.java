fef527a3f37116d3cbe0afdc502f877ba867911a
Dikendev <diken.dev@gmail.com>
2023-03-16 16:44:05 -0300

    fixed Tomador model

--- Back-End/src/main/java/nfsetimbo/capivaratech/model/Tomador.java
+++ Back-End/src/main/java/nfsetimbo/capivaratech/model/Tomador.java
@@ -1,55 +1,50 @@
 package nfsetimbo.capivaratech.model;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlType;
+import jakarta.persistence.Column;
 import lombok.Data;
 import nfsetimbo.capivaratech.bean.enums.Tipo;
 
 import java.io.Serializable;
 
 @Data
-@XmlType(propOrder = {"tipo", "cpfcnpj", "nome_razao_social"})
-@XmlAccessorType(XmlAccessType.FIELD)
 public class Tomador implements Serializable {
 
-    @XmlElement(name = "tipo")
+    @Column(name = "tipo")
     protected Tipo tipo;
-    @XmlElement(name = "cpfcnpj")
+    @Column(name = "cpfcnpj")
     protected int cpfCnpj;
-    @XmlElement(name = "nome_razao_social")
+    @Column(name = "nome_razao_social")
     protected String razaoSocial;
 
-    @XmlElement(name = "logradouro")
+    @Column(name = "logradouro")
     protected String logradouro;
 
-    @XmlElement(name = "email")
+    @Column(name = "email")
     protected String email;
 
-    @XmlElement(name = "numero_residencia")
+    @Column(name = "numero_residencia")
     protected int numero_residencia;
-    @XmlElement(name = "bairro")
+    @Column(name = "bairro")
     protected String bairro;
 
-    @XmlElement(name = "cidade")
+    @Column(name = "cidade")
     protected int cidade;
-    @XmlElement(name = "cep")
+    @Column(name = "cep")
     protected int cep;
-    @XmlElement(name = "ddd_fone_comercial")
+    @Column(name = "ddd_fone_comercial")
     protected int ddd_fone_comercial;
-    @XmlElement(name = "fone_comercial")
+    @Column(name = "fone_comercial")
     protected int fone_comercial;
-    @XmlElement(name = "ddd_fone_residencial")
+    @Column(name = "ddd_fone_residencial")
     protected int ddd_fone_residencial;
 
-    @XmlElement(name = "fone_residencial")
+    @Column(name = "fone_residencial")
     protected int foneResidencial;
 
-    @XmlElement(name = "ddd_fax")
+    @Column(name = "ddd_fax")
     protected int dddFax;
 
-    @XmlElement(name = "fone_fax")
+    @Column(name = "fone_fax")
     protected int foneFax;
 
     public Tomador() {}
