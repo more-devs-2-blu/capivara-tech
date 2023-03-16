9151aa73ce7d65d7fd5757dce569baff9ad25819
Dikendev <diken.dev@gmail.com>
2023-03-16 10:43:04 -0300

    added Lista itens class an xml attr

--- Back-End/src/main/java/nfsetimbo/capivaratech/model/ListaNfse.java
+++ Back-End/src/main/java/nfsetimbo/capivaratech/model/ListaNfse.java
@@ -1,100 +1,54 @@
 package nfsetimbo.capivaratech.model;
 
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.*;
+import lombok.Data;
 import nfsetimbo.capivaratech.bean.enums.TributaMunicipioPrestador;
 
 import java.io.Serializable;
 
+@XmlType(propOrder = {"tributa_municipio_prestador","codigo_local_prestacaoServico",
+        "unidade_codigo", "unidade_quantidade",
+        "unidade_valor_unitario", "codigo_item_lista_servico",
+        "descritivo", "aliquota_item_lista_servico", "situacao_tributaria", "valor_tributavel"})
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+@Data
 public class ListaNfse implements Serializable {
+
+   @XmlElement(name = "tributa_municipio_prestador")
    private TributaMunicipioPrestador tributaMunicipioPrestador;
+   @XmlElement(name = "codigo_local_prestacao_servico")
    private int codigoLocalPrestacaoServico = 8357;
+   @XmlElement(name = "unidade_codigo" )
    private int unidadeCodigo = 43;
+   @XmlElement(name = "unidade_quantidade")
    private int unidadeQuantidade = 1;
-   private double unidadeValorUnitario = 1.0;
-   private  int  codigoItemListaServico = 9;
+   @XmlElement(name = "unidade_valor_unitario")
+   private String  unidadeValorUnitario;
+   @XmlElement(name = "codigo_item_lista_servico")
+   private  int  codigoItemListaServico;
+   @XmlElement(name = "descritivo")
    private  String descritivo = "teste";
-   private double aliquotaItemListaServico = 5.00;
-   private double situacaoTributaria;
-   private double valorTributavel;
+   @XmlElement(name = "aliquota_item_lista_servico")
+   private String aliquotaItemListaServico;
+   @XmlElement(name = "situacao_tributaria")
+   private String situacaoTributaria;
+   @XmlElement(name = "valor_tributavel")
+   private String valorTributavel;
 
-   public TributaMunicipioPrestador getTributaMunicipioPrestador() {
-      return tributaMunicipioPrestador;
-   }
+   public ListaNfse() {}
 
-   public void setTributaMunicipioPrestador(TributaMunicipioPrestador tributaMunicipioPrestador) {
+   public ListaNfse(TributaMunicipioPrestador tributaMunicipioPrestador, int codigoLocalPrestacaoServico, int unidadeCodigo, int unidadeQuantidade, String unidadeValorUnitario, int codigoItemListaServico, String descritivo, String aliquotaItemListaServico, String situacaoTributaria, String valorTributavel) {
       this.tributaMunicipioPrestador = tributaMunicipioPrestador;
-   }
-
-   public int getCodigoLocalPrestacaoServico() {
-      return codigoLocalPrestacaoServico;
-   }
-
-   public void setCodigoLocalPrestacaoServico(int codigoLocalPrestacaoServico) {
       this.codigoLocalPrestacaoServico = codigoLocalPrestacaoServico;
-   }
-
-   public int getUnidadeCodigo() {
-      return unidadeCodigo;
-   }
-
-   public void setUnidadeCodigo(int unidadeCodigo) {
       this.unidadeCodigo = unidadeCodigo;
-   }
-
-   public int getUnidadeQuantidade() {
-      return unidadeQuantidade;
-   }
-
-   public void setUnidadeQuantidade(int unidadeQuantidade) {
       this.unidadeQuantidade = unidadeQuantidade;
-   }
-
-   public double getUnidadeValorUnitario() {
-      return unidadeValorUnitario;
-   }
-
-   public void setUnidadeValorUnitario(double unidadeValorUnitario) {
       this.unidadeValorUnitario = unidadeValorUnitario;
-   }
-
-   public int getCodigoItemListaServico() {
-      return codigoItemListaServico;
-   }
-
-   public void setCodigoItemListaServico(int codigoItemListaServico) {
       this.codigoItemListaServico = codigoItemListaServico;
-   }
-
-   public String getDescritivo() {
-      return descritivo;
-   }
-
-   public void setDescritivo(String descritivo) {
       this.descritivo = descritivo;
-   }
-
-   public double getAliquotaItemListaServico() {
-      return aliquotaItemListaServico;
-   }
-
-   public void setAliquotaItemListaServico(double aliquotaItemListaServico) {
       this.aliquotaItemListaServico = aliquotaItemListaServico;
-   }
-
-   public double getSituacaoTributaria() {
-      return situacaoTributaria;
-   }
-
-   public void setSituacaoTributaria(double situacaoTributaria) {
       this.situacaoTributaria = situacaoTributaria;
-   }
-
-   public double getValorTributavel() {
-      return valorTributavel;
-   }
-
-   public void setValorTributavel(double valorTributavel) {
       this.valorTributavel = valorTributavel;
    }
 }
